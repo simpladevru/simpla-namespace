5bad3c1fb3c9fcac89341f34b3d89fcef9b908ec
Balyhin <simpladev@yandex.ru>
2020-11-04 14:10:09 +0300

    0411201410

---
+++ vendor/composer/installed.php
@@ -0,0 +1,141 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'simpla/cms',
+  ),
+  'versions' => 
+  array (
+    'illuminate/collections' => 
+    array (
+      'pretty_version' => 'v8.13.0',
+      'version' => '8.13.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e61e796ff98e19eac5e6735ea6abfa66f4a2f6db',
+    ),
+    'illuminate/contracts' => 
+    array (
+      'pretty_version' => 'v8.13.0',
+      'version' => '8.13.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c23428149639d5257916ad10d390cdbeffe4d504',
+    ),
+    'illuminate/macroable' => 
+    array (
+      'pretty_version' => 'v8.13.0',
+      'version' => '8.13.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '300aa13c086f25116b5f3cde3ca54ff5c822fb05',
+    ),
+    'mpociot/pipeline' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3584db4a0de68067b2b074edfadf6a48b5603a6b',
+    ),
+    'pclzip/pclzip' => 
+    array (
+      'pretty_version' => '2.8.2',
+      'version' => '2.8.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '19dd1de9d3f5fc4d7d70175b4c344dee329f45fd',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b7ce3b176482dbbc1245ebf52b181af44c2cf55f',
+    ),
+    'psr/simple-cache' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '408d5eafb83c57f6365a3ca330ff23aa4a5fa39b',
+    ),
+    'simpla/cms' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'smarty/smarty' => 
+    array (
+      'pretty_version' => 'v3.1.36',
+      'version' => '3.1.36.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fd148f7ade295014fff77f89ee3d5b20d9d55451',
+    ),
+    'symfony/filesystem' => 
+    array (
+      'pretty_version' => 'v5.1.8',
+      'version' => '5.1.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'df08650ea7aee2d925380069c131a66124d79177',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f4ba089a5b6366e453971d3aad5fe8e897b37f41',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '39d483bdf39be819deabf04ec872eb0b2410b531',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e70aa8b064c5b72d3df2abd5ab1e90464ad009de',
+    ),
+    'symfony/var-dumper' => 
+    array (
+      'pretty_version' => 'v5.1.8',
+      'version' => '5.1.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4e13f3fcefb1fcaaa5efb5403581406f4e840b9a',
+    ),
+  ),
+);
